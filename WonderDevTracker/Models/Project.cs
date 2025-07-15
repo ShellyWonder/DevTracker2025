8d913cef649ebbe193fbae6796eeb14e81eb198f
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-15 04:50:34 -0500

    modified Project.cs; built out Ticket.cs.

--- WonderDevTracker/Models/Project.cs
+++ WonderDevTracker/Models/Project.cs
@@ -6,19 +6,35 @@ namespace WonderDevTracker.Models
     public class Project
 
     {
+        private DateTimeOffset _created;
+        private DateTimeOffset _startDate;
+        private DateTimeOffset _endDate;
+        
         public int Id { get; set; }
 
         [StringLength(100, ErrorMessage = "The name cannot exceed 100 characters.")]
         [Required]
         public string? Name { get; set; }
-        public string? Description { get; set; } 
+        public string? Description { get; set; }
 
-        public DateTimeOffset Created { get; set; } 
+        public DateTimeOffset Created
+        {
+            get => _created;
+            set => _created = value.ToUniversalTime();
+        }
 
-        public DateTimeOffset? StartDate { get; set; }
-     
-        public DateTime? EndDate { get; set; }
-        public ProjectPriority? Priority { get; set; } 
+        public DateTimeOffset StartDate 
+        {
+            get => _startDate; 
+            set => _startDate = value.ToUniversalTime();
+        }
+
+       public DateTimeOffset EndDate 
+        {
+            get => _endDate; 
+            set => _endDate = value.ToUniversalTime();
+        }
+        public ProjectPriority? Priority { get; set; }
 
         public bool Archived { get; set; } = false;
 
