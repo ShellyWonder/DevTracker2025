f75edadfb0ee586b600177e8cb37410238eb66ad
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-15 01:28:16 -0500

    Created Company and Project models

---
+++ WonderDevTracker/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WonderDevTracker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        [StringLength(100, ErrorMessage = "The name cannot exceed 100 characters.")]
+        [Required]
+        public string? Name { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public Guid? ImageId { get; set; }
+
+        public virtual ImageUpload? Image { get; set; }
+
+        public virtual ICollection<ApplicationUser>? Members { get; set; } = [];
+        public virtual ICollection<Project>? Projects { get; set; } = [];
+        public virtual ICollection<Invite>? Invites { get; set; } = [];
+
+        
+    }
+
+}
---
+++ WonderDevTracker/Models/Project.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WonderDevTracker.Client.Models.Enums;
+
+namespace WonderDevTracker.Models
+{
+    public class Project
+
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, ErrorMessage = "The name cannot exceed 100 characters.")]
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; } 
+
+        public DateTimeOffset Created { get; set; } 
+
+        public DateTimeOffset? StartDate { get; set; }
+     
+        public DateTime? EndDate { get; set; }
+        public ProjectPriority? Priority { get; set; } 
+
+        public bool Archived { get; set; } = false;
+
+        public Guid? ImageId { get; set; }
+        public virtual ImageUpload? Image { get; set; }
+
+        //foreign key to Company
+        public int CompanyId { get; set; }
+        public virtual Company? Company { get; set; }
+        public virtual ICollection<ApplicationUser>? Members { get; set; } = [];
+
+        public virtual ICollection<Ticket> Tickets { get; set; } = [];
+    }
+}
