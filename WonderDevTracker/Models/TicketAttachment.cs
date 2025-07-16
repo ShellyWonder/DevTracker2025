e5e4442dfb22f1216305353bda68e5cf2eef59f1
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-15 20:40:13 -0500

    Built out TicketAttachment.cs

---
+++ WonderDevTracker/Models/TicketAttachment.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WonderDevTracker.Models
+{
+    public class TicketAttachment
+    {
+        private DateTimeOffset _created;
+        public int Id { get; set; }
+
+        [Required]
+        public string? FileName { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTimeOffset Created
+        {
+            get => _created;
+            set => _created = value.ToUniversalTime();
+        }
+
+        //navigation properties --all strings are nullable
+        public Guid UploadId { get; set; }
+        public virtual FileUpload? Upload { get; set; }
+        public int TicketId { get; set; }
+        public virtual Ticket? Ticket { get; set; }
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
