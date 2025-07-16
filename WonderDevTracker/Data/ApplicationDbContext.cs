e7cbe78467d255edd272e1ac842ad37980c04f5c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-16 01:20:57 -0500

    Init commit to database

--- WonderDevTracker/Data/ApplicationDbContext.cs
+++ WonderDevTracker/Data/ApplicationDbContext.cs
@@ -6,6 +6,14 @@ namespace WonderDevTracker.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        public DbSet<ImageUpload> Images { get; set; }
+        public DbSet<FileUpload> Uploads { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketComment> Comments { get; set; }
+        public DbSet<TicketAttachment> Attachments { get; set; }
+        public DbSet<TicketHistory> History { get; set; }
+        public DbSet<Invite> Invites { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
     }
 }
