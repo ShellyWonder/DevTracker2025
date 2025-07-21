81bbaffbec8d6a216324415c0daacdff0cb25fc9
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-21 09:14:45 -0500

    Implemented AuthenticatedComponentBase to address code duplication.

---
+++ WonderDevTracker.Client/BaseComponents/AuthenticatedComponentBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using WonderDevTracker.Client.Helpers;
+
+namespace WonderDevTracker.Client.BaseComponents
+{
+    public abstract class AuthenticatedComponentBase : ComponentBase
+    {
+        [CascadingParameter]
+        public Task<AuthenticationState> AuthStateTask { get; set; } = default!;
+
+        protected UserInfo? UserInfo { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            UserInfo = await UserInfoHelper.GetUserInfoAsync(AuthStateTask);
+            await OnInitializedWithAuthAsync();
+        }
+
+        /// <summary>
+        /// Override this method instead of OnInitializedAsync when inheriting.
+        /// </summary>
+        protected virtual Task OnInitializedWithAuthAsync() => Task.CompletedTask;
+
+
+    }
+}
