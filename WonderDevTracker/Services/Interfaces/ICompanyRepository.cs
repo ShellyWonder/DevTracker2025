ad6d1348d9a2aa0eb8e2f407ca86dae967a94586
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-24 07:26:11 -0500

    Added interface for CompanyRepository

---
+++ WonderDevTracker/Services/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,23 @@
+﻿//ICompanyRepository
+
+
+//ICompanyRepository
+
+using WonderDevTracker.Models;
+
+namespace WonderDevTracker.Services.Interfaces
+{
+    public interface ICompanyRepository
+    {
+
+        public Task<Company?> GetCompanyInfoByIdAsync(int companyId);
+        public Task<List<ApplicationUser>>GetAllMembersAsync(int companyId);
+        public Task<List<Project>>GetAllProjectsAsync(int companyId);
+        public Task<List<Ticket>>GetAllTicketsAsync(int companyId);
+        public Task<List<Invite>>GetAllInvitesAsync(int companyId);
+
+        
+    }
+
+}
+
