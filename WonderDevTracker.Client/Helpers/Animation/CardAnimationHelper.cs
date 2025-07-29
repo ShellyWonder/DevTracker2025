8b42a7358fb5f81283e010031f17fda54c54071b
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-29 12:50:23 -0500

    Added UI enhancements to ProjectCard and ProjectIndex ; Added CardAnimationHelper.cs to assist with card fade-in animation.

---
+++ WonderDevTracker.Client/Helpers/Animation/CardAnimationHelper.cs
@@ -0,0 +1,24 @@
+﻿using WonderDevTracker.Client.Models.DTOs;
+
+namespace WonderDevTracker.Client.Helpers.Animation
+{
+    public class CardAnimationHelper(IEnumerable<ProjectDTO> projects)
+    {
+
+        public class ProjectWithIndex
+        {
+            public ProjectDTO Project { get; set; } = default!;
+            public int Index { get; set; }
+        }
+        private readonly IEnumerable<ProjectDTO>? _projects = projects;
+
+        public List<ProjectWithIndex> GetProjectsWithIndex()
+        {
+            return [.. _projects!.Select((p, i) => new ProjectWithIndex
+            {
+                Project = p,
+                Index = i
+            })];
+        }
+    }
+}
