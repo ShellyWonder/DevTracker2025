5549584902ddc6277466baf399ec71a1f3e65875
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-30 14:56:27 -0500

    Added helper properties in ProjectDTO for DateTime type conversion; modified affected razor components;

--- WonderDevTracker.Client/Models/DTOs/ProjectDTO.cs
+++ WonderDevTracker.Client/Models/DTOs/ProjectDTO.cs
@@ -6,9 +6,9 @@ namespace WonderDevTracker.Client.Models.DTOs
 {
     public class ProjectDTO
     {
-        private DateTimeOffset _startDate;
         private DateTimeOffset _created;
-        private DateTimeOffset _endDate;
+        private DateTimeOffset? _startDate;
+        private DateTimeOffset? _endDate;
 
         public int Id { get; set; }
 
@@ -17,22 +17,25 @@ namespace WonderDevTracker.Client.Models.DTOs
         public string? Name { get; set; }
         public string? Description { get; set; }
 
+       
         public DateTimeOffset Created
         {
             get => _created;
             set => _created = value.ToUniversalTime();
         }
 
-        public DateTimeOffset StartDate
+        [Required]
+        public DateTimeOffset? StartDate
         {
             get => _startDate;
-            set => _startDate = value.ToUniversalTime();
+            set => _startDate = value?.ToUniversalTime();
         }
 
-        public DateTimeOffset EndDate
+        [Required]
+        public DateTimeOffset? EndDate
         {
             get => _endDate;
-            set => _endDate = value.ToUniversalTime();
+            set => _endDate = value?.ToUniversalTime();
         }
         public ProjectPriority Priority { get; set; }
 
@@ -43,6 +46,23 @@ namespace WonderDevTracker.Client.Models.DTOs
 
         public ICollection<TicketDTO> Tickets { get; set; } = [];
 
+        #region Helper Properties 
+        /// <summary>
+        /// Helpers to convert DateTimeOffset to DateTime in UTC
+        /// Necessary for MudBlazor DatePicker to work correctly
+        /// </summary>
+        public DateTime? StartDateTime
+        {
+            get => StartDate?.DateTime;
+            set => StartDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : null;
+        }
+
+        public DateTime? EndDateTime
+        {
+            get => EndDate?.DateTime;
+            set => EndDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : null;
+        }
+        #endregion
     }
-    
+
 }
