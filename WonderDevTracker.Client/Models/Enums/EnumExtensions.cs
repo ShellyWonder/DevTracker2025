57431d036fa875e9c413ba9bfe77b99b3c33d9a0
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-08-06 01:31:57 -0500

    Added a dictionary to map generics; instituted generic extension method in priorityChip.razor and ProjectTicketTable.razor; added pagination to ticket table.

--- WonderDevTracker.Client/Models/Enums/EnumExtensions.cs
+++ WonderDevTracker.Client/Models/Enums/EnumExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MudBlazor;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace WonderDevTracker.Client.Models.Enums
@@ -12,9 +13,51 @@ namespace WonderDevTracker.Client.Models.Enums
                                        .FirstOrDefault()?
                                        .GetCustomAttribute<DisplayAttribute>()?.GetName();
 
-            if (string.IsNullOrEmpty(displayName)) return value.ToString();
-           
-            return displayName;
+            return string.IsNullOrEmpty(displayName) ? value.ToString() : displayName;
+ 
+        }
+
+        private static readonly Dictionary<Type, Dictionary<Enum, Color>> _enumColorMap = new()
+        {
+            {
+                typeof(ProjectPriority), new()
+                {
+                    { ProjectPriority.Low, Color.Tertiary },
+                    { ProjectPriority.Medium, Color.Info },
+                    { ProjectPriority.High, Color.Warning },
+                    { ProjectPriority.Urgent, Color.Error }
+                }
+            },
+            {
+                typeof(TicketPriority), new()
+                {
+                    { TicketPriority.Low, Color.Tertiary },
+                    { TicketPriority.Medium, Color.Info },
+                    { TicketPriority.High, Color.Warning },
+                    { TicketPriority.Urgent, Color.Error }
+                }
+            },
+            {
+                typeof(TicketStatus), new()
+                {
+                    { TicketStatus.New, Color.Tertiary },
+                    { TicketStatus.InDevelopment, Color.Info },
+                    { TicketStatus.InTesting, Color.Warning },
+                    { TicketStatus.Resolved, Color.Success }
+                }
+            }
+        };
+        public static Color GetColor<TEnum>(this TEnum? value) where TEnum : struct, Enum
+        {
+            if (value is null) return Color.Default;
+
+            if (_enumColorMap.TryGetValue(typeof(TEnum), out var map)
+                && map.TryGetValue(value.Value, out var color))
+            {
+                return color;
+            }
+
+            return Color.Default;
         }
     }
 }
