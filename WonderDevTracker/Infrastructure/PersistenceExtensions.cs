c5d742c1f98781812990cf8900165aba48cc3933
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-09-14 03:40:18 -0500

    Added additional DbContext for use with UserManager

--- WonderDevTracker/Infrastructure/PersistenceExtensions.cs
+++ WonderDevTracker/Infrastructure/PersistenceExtensions.cs
@@ -1,25 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WonderDevTracker.Data;
 
-namespace WonderDevTracker.Infrastructure
+namespace WonderDevTracker.Infrastructure;
+
+public static class PersistenceExtensions
 {
-    
-    public static class PersistenceExtensions
+    /// <summary>
+    /// Used to house the db context registration for dependency injection
+    /// </summary>
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration config)
     {
-        /// <summary>
-        /// Used to house the db context registration for dependency injection
-        /// </summary>
-        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration config)
-        {
-            var connectionString = DataUtility.GetConnectionString(config) ?? throw new InvalidOperationException("Connection string 'DbConnection' not found.");
-            services.AddDbContextFactory<ApplicationDbContext>(options =>
-                options.UseNpgsql(
-                    connectionString,
-                    options => options.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
-                    ));
+        var connectionString = DataUtility.GetConnectionString(config) ?? throw new InvalidOperationException("Connection string 'DbConnection' not found.");
+        
+        services.AddDbContextFactory<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                connectionString,
+                options => options.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+                ));
+
+        //  Scoped DbContext for Identity (UserManager/Stores)
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString,
+                b => b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 
 
-            return services;
-        }
+        return services;
     }
 }
