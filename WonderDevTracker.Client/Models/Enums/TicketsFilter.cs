6a3a1a04b3f93373b7790189928fe33081ab1a0d
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-10-19 17:43:58 -0500

    Created TicketsFilter(enum) to provide filters for TicketsController. GetTickets API endpoint

---
+++ WonderDevTracker.Client/Models/Enums/TicketsFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WonderDevTracker.Client.Models.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TicketsFilter
+    {
+        Open,
+        Assigned,
+        Resolved,
+        Archived
+    }
+}
