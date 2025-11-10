890722955e0d0da4a0dd51e243d4c351f630f8df
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-11-09 19:16:39 -0600

    modified UploadsController and AttachmentCard to enable TicketAttachment download feature;

--- WonderDevTracker/Controllers/UploadsController.cs
+++ WonderDevTracker/Controllers/UploadsController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
+using WonderDevTracker.Client;
+using WonderDevTracker.Client.Helpers;
 using WonderDevTracker.Data;
 using WonderDevTracker.Models;
 
@@ -17,10 +20,29 @@ namespace WonderDevTracker.Controllers
         public async Task<IActionResult> GetImage(Guid id)
         {
             FileUpload? image = await _context.Uploads.FirstOrDefaultAsync(i => i.Id == id);
-            if (image is null) return NotFound();
+            //prevents this endpoints from collecting Attachment image
+            if (image is null || await _context.Attachments.AnyAsync(a => a.UploadId ==id)) return NotFound();
 
             //return byte array to the frontend for conversion into a src tag displaying on the page
             return File(image.Data!, image.Type!);
+        }
+
+        //Current use: TicketAttachments.ToDTO() uses this endpoint to get the attachment URL
+        [SwaggerIgnore]
+        [HttpGet("/api/attachments/{uploadId:guid}")]
+        [Authorize]
+        public async Task<IActionResult> DownloadAttachment([FromRoute] Guid uploadId)
+        {
+            UserInfo userInfo = UserInfoHelper.GetUserInfo(User)!;
+            TicketAttachment? attachment = await _context.Attachments
+                .Include(a => a.Upload)
+                .FirstOrDefaultAsync(a => a.UploadId == uploadId
+                                                   && a.Ticket!.Project!.CompanyId == userInfo.CompanyId);
+            if (attachment is null) return NotFound();
+            //Prevents the file being opened in the browser
+            Response.Headers.TryAdd("Content-Disposition", $"attachment, filename={attachment.FileName}");
+            return File(attachment.Upload!.Data!, attachment.Upload.Type!, attachment.FileName);
+
         }
     }
 }
