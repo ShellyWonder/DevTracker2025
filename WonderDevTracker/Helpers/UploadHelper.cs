6153aca43802d654479a3c7b40499922ee677e60
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-11-11 02:34:17 -0600

    Renamed file to reflect role in file upload feature;

--- WonderDevTracker/Helpers/UploadHelper.cs
+++ WonderDevTracker/Helpers/UploadHelper.cs
@@ -3,24 +3,24 @@ using WonderDevTracker.Models;
 
 namespace WonderDevTracker.Helpers
 {
-    public static class ImageHelper
+    public static class UploadHelper
     {
         public static readonly string DefaultProfilePictureUrl = "/img/ProfilePlaceHolder.png";
 
-        public static async Task<FileUpload> GetImageUploadAsync(IFormFile file)
+        public static async Task<FileUpload> GetFileUploadAsync(IFormFile file)
         {
             using var ms = new MemoryStream();
             await file.CopyToAsync(ms);
             byte[] data = ms.ToArray();
-            if (ms.Length > BrowserFileHelper.MaxFileSize) throw new Exception("The image is too large.");
+            if (ms.Length > BrowserFileHelper.MaxFileSize) throw new IOException("The image is too large.");
 
-            FileUpload imageUpload = new()
+            FileUpload upload = new()
             {
                 Id = Guid.NewGuid(),
                 Data = data,
                 Type = file.ContentType
             };
-            return imageUpload;
+            return upload;
         }
     }
 }
