104e9624fffece613e4da2f2077b71f400dc0645
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-11-21 11:00:44 -0600

    Refining InviteDTO.cs by eliminating unnecessary properties and adding API description;

--- WonderDevTracker.Client/Models/DTOs/InviteDTO.cs
+++ WonderDevTracker.Client/Models/DTOs/InviteDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WonderDevTracker.Client.Models.DTOs
 {
@@ -8,45 +9,61 @@ namespace WonderDevTracker.Client.Models.DTOs
         private DateTimeOffset _inviteDate;
         private DateTimeOffset? _joinDate;
 
+        [Description("Unique identifier for the invite")]
         public int Id { get; set; }
 
+        [Description("Date the invite was sent in UTC")]
         public DateTimeOffset InviteDate
         {
             get => _inviteDate;
             set => _inviteDate = value.ToUniversalTime();
         }
 
+        [Description("Date the invite was accepted in UTC")]
         public DateTimeOffset? JoinDate
         {
             get => _joinDate;
             set => _joinDate = value?.ToUniversalTime();
         }
-        public Guid CompanyToken { get; set; } = Guid.NewGuid();
+       
 
         [Required, EmailAddress]
+        [Description("Invitee email address")]
         public string? InviteeEmail { get; set; }
 
         [Required]
+        [Description("Invitee first name")]
         public string? InviteeFirstName { get; set; }
+
+        [Required]
+        [Description("Invitee last name")]
         public string? InviteeLastName { get; set; }
 
+        [Required]
+        [Description("Optional message in email body")]
         public string? Message { get; set; }
 
+        [Description("Indicates whether the invite is currently valid - invites are valid for 7 days.")]
         public bool IsValid { get; set; } = false;
 
         //navigation properties
-        public int CompanyId { get; set; }
-        public CompanyDTO? Company { get; set; }
-
+        [Description("The project on whose behalf the Company Administration is sending the invite")]
         public ProjectDTO? Project { get; set; }
+
+        [Description("The  Id of the project on whose behalf the Company Administration is sending the invite")]
         public int ProjectId { get; set; }
 
-
         [Required]
+        [Description("Id of the Company Administration sending the invite")]
         public string? InvitorId { get; set; }
+
+        [Description("Id of the Company Administration sending the invite")]
         public  AppUserDTO? Invitor { get; set; }
 
+        [Description("Id of the user who accepted the invite")]
         public string? InviteeId { get; set; }
+
+        [Description("The user who accepted the invite")]
         public  AppUserDTO? Invitee { get; set; }
 
 
