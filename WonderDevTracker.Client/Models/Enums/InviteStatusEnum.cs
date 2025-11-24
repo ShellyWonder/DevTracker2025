ac936a5a56c11ae325b687c65c5280a1a799b408
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-11-24 11:34:01 -0600

    Implemented UI feature to resend and cancel pending invites

---
+++ WonderDevTracker.Client/Models/Enums/InviteStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WonderDevTracker.Client.Models.Enums
+{
+    public enum InviteStatusEnum
+    {
+        [Display(Name = "Pending")]
+        Pending,
+
+        [Display(Name = "Accepted")]
+        Accepted,
+
+        [Display(Name = "Cancelled / Expired")]
+        Cancelled
+    }
+}
