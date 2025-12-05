4c3c44ef94e6663393f751d761b993db2ebf3a52
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-12-05 14:46:41 -0600

    Added ServerAuthenticatedComponentBase to handle Auth functions in InteractiveServer render mode.

---
+++ WonderDevTracker/Components/Base/ServerAuthenticatedComponentBase .cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace WonderDevTracker.Components.Base
+{
+    public abstract class ServerAuthenticatedComponentBase : ComponentBase
+    {
+        [CascadingParameter]
+        public Task<AuthenticationState> AuthStateTask { get; set; } = default!;
+
+        protected ClaimsPrincipal? AuthUser { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthStateTask;
+            AuthUser = authState.User;
+            await OnInitializedWithAuthAsync();
+        }
+
+        /// <summary>
+        /// Override this instead of OnInitializedAsync in derived components.
+        /// </summary>
+        protected virtual Task OnInitializedWithAuthAsync() => Task.CompletedTask;
+
+        protected bool IsAuthenticated => AuthUser?.Identity?.IsAuthenticated == true;
+    }
+}
