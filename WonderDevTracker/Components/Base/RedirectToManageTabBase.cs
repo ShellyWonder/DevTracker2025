da5efde3947fbde552c9229660c43477dc754cb7
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-12-07 20:29:53 -0600

    Created RedirectToManageTabBase in order to track Tab positions for accountSettings panels; Implemented Tab tracking index on individual account pages so that routes are redirected from the original pages to the panels.

---
+++ WonderDevTracker/Components/Base/RedirectToManageTabBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WonderDevTracker.Components.Base
+{
+    
+    public abstract class RedirectToManageTabBase : ComponentBase
+    {
+        /// <summary>
+        /// Override this in children to indicate which tab to select.
+        /// </summary>
+        /// 
+        [Inject] private NavigationManager Nav { get; set; } = default!;
+        protected abstract int TargetTabIndex { get; }
+
+        protected override void OnInitialized()
+        {
+            // Redirect to Index with a query parameter instructing it which tab to activate
+            var uri = $"/Account/Manage?tab={TargetTabIndex}";
+            Nav.NavigateTo(uri, forceLoad: false);
+        }
+    }
+}
