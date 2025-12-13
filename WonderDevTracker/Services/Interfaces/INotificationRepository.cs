5e4b46058e170ebb090bd153e1f20dc9ababaf5c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-12-13 16:19:08 -0600

    Scaffolded INotificationRepository and NotificationRepository;

---
+++ WonderDevTracker/Services/Interfaces/INotificationRepository.cs
@@ -0,0 +1,13 @@
+﻿using WonderDevTracker.Models;
+
+namespace WonderDevTracker.Services.Interfaces
+{
+    public interface INotificationRepository
+    {
+        public Task <List<Notification>>GetByRecipientAsync(string recipientId);
+        public Task <int> GetUnreadCountAsync(string recipientId);
+        public Task<Notification?> GetNotificationById(int id);
+        public Task AddRangeAsync(IEnumerable<Notification> notifications);
+        public Task MarkViewedAsync(int notificationId, string recipientId);
+    }
+}
---
+++ WonderDevTracker/Services/Repositories/NotificationRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WonderDevTracker.Data;
+using WonderDevTracker.Models;
+using WonderDevTracker.Services.Interfaces;
+
+namespace WonderDevTracker.Services.Repositories
+{
+    public class NotificationRepository(IDbContextFactory<ApplicationDbContext> contextFactory) : INotificationRepository
+    {
+        
+        public Task AddRangeAsync(IEnumerable<Notification> notifications)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetByRecipientAsync(string recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Notification?> GetNotificationById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetUnreadCountAsync(string recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MarkViewedAsync(int notificationId, string recipientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
