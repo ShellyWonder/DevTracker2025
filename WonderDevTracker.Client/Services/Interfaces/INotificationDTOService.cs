2bd0679fc8f2942668c8037837206139b1934422
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-12-17 12:17:49 -0600

    Documented Notification interfaces;

--- WonderDevTracker.Client/Services/Interfaces/INotificationDTOService.cs
+++ WonderDevTracker.Client/Services/Interfaces/INotificationDTOService.cs
@@ -4,13 +4,70 @@ namespace WonderDevTracker.Client.Services.Interfaces
 {
     public interface INotificationDTOService
     {
-        Task<List<NotificationDTO>> GetForCurrentUserAsync(UserInfo userInfo, int take = 50);
-        Task<int> GetUnreadCountForCurrentUserAsync(UserInfo userInfo);
 
-        Task MarkViewedAsync(int notificationId, UserInfo userInfo);
+        /// <summary>
+        /// Create Notification
+        /// </summary>
+        /// <param name="notificationDTO">An object containing the details of the notification to create. Cannot be null.</param>
+        /// <remarks>Asynchronously creates a new notification based on the specified data transfer object. </remarks>
+        public Task CreateNotificationAsync(NotificationDTO notificationDTO);
 
-        // Admin-only
-        Task<List<NotificationDTO>> GetForUserAsAdminAsync(string targetUserId, UserInfo adminUserInfo, int take = 50);
+       /// <summary>
+       /// Asynchronously retrieves a list of notifications for the specified user.
+       /// </summary>
+       /// <param name="userInfo">The user information identifying the current user for whom notifications are to be retrieved. Cannot be null.</param>
+       /// <param name="take">The maximum number of notifications to return. Must be a positive integer. The default value is 20.</param>
+       /// <returns>A task that represents the asynchronous operation. The task result contains a list of notifications for the
+       /// specified user. The list will be empty if no notifications are found.</returns>
+        public Task<List<NotificationDTO>> GetForCurrentUserAsync(UserInfo userInfo, int take = 20);
+
+
+        /// <summary>
+        /// Asynchronously retrieves the number of unread messages for the specified user.
+        /// </summary>
+        /// <param name="userInfo">The user information identifying the current user for whom to retrieve the unread message count. Cannot be
+        /// null.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the number of unread messages
+        /// for the specified user.</returns>
+        public Task<int> GetUnreadCountForCurrentUserAsync(UserInfo userInfo);
+
+        /// <summary>
+        /// Marks the specified notification as viewed by the given user asynchronously.
+        /// </summary>
+        /// <param name="notificationId">The unique identifier of the notification to mark as viewed.</param>
+        /// <param name="userInfo">The user for whom the notification is being marked as viewed. Cannot be null.</param>
+        /// <remarks>A task that represents the asynchronous operation.</remarks>
+        public Task MarkViewedAsync(int notificationId, UserInfo userInfo);
+
+        /// <summary>
+        /// Archive Notification
+        /// </summary>
+        /// <param name="notificationId">Notification id to archive.</param>
+        /// <param name="recipientId">Recipient id for whom the notification will be archived. Cannot be null or empty.</param>
+        /// <remarks> Asynchronously archives(soft delete) a notification for a specified recipient. May be restored by the recepient or company admin.</remarks>
+        public Task ArchiveNotificationAsync(int notificationId, string recipientId);
+
+        /// <summary>
+        /// Restore Archived Notification
+        /// </summary>
+        /// <param name="notificationId">Id of the notification to restore.</param>
+        /// <param name="recipientId">Recipient id for whom the notification will be restored. Cannot be null or empty.</param>
+        /// <remarks>Restores a previously archived(soft delete) notification for the specified recipient asynchronously.</remarks>
+        public Task RestoreNotificationAsync(int notificationId, string recipientId);
+
+
+        /// <summary>
+        /// Admin Get Notifications For User
+        /// </summary>
+        /// <remarks>Retrieves a list of notifications for the specified user as an administrator. Ensure that
+        /// appropriate permissions are enforced before calling this method.</remarks>
+        /// <param name="targetUserId">The unique identifier of the user whose notifications are to be retrieved. Cannot be null or empty.</param>
+        /// <param name="adminUserInfo">Information about the administrator performing the operation. Must represent a user with administrative
+        /// privileges. Cannot be null.</param>
+        /// <param name="take">The maximum number of notifications to retrieve. Must be a positive integer. The default value is 20.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of notifications for the
+        /// specified user. The list will be empty if the user has no notifications.</returns>
+        public Task<List<NotificationDTO>> GetForUserAsAdminAsync(string targetUserId, UserInfo adminUserInfo, int take = 20);
     }
 }
 
--- WonderDevTracker/Services/Interfaces/INotificationRepository.cs
+++ WonderDevTracker/Services/Interfaces/INotificationRepository.cs
@@ -5,14 +5,74 @@ namespace WonderDevTracker.Services.Interfaces
     public interface INotificationRepository
     {
 
+        /// <summary>
+        /// Asynchronously adds a notification to the system.
+        /// </summary>
+        /// <param name="notification">The notification to add. Cannot be null.</param>
+        /// <remarks>A task that represents the asynchronous add operation.</remarks>
         public Task AddNotificationAsync(Notification notification);
-        public Task <List<Notification>>GetByRecipientAsync(string recipientId, int take);
-        public Task <int> GetUnreadCountAsync(string recipientId);
+
+        /// <summary>
+        /// Asynchronously retrieves a list of notifications for the specified recipient.   
+        /// </summary>
+        /// <param name="recipientId">The unique identifier of the recipient whose notifications are to be retrieved. Cannot be null or empty.</param>
+        /// <param name="take">The maximum number of notifications to return. Must be greater than zero.</param>
+        /// <remarks>A task that represents the asynchronous operation. The task result contains a list of notifications for the
+        /// specified recipient. The list will be empty if no notifications are found.</remarks>
+        public Task<List<Notification>> GetByRecipientAsync(string recipientId, int take);
+
+        /// <summary>
+        /// Asynchronously retrieves the number of unread messages for the specified recipient.
+        /// </summary>
+        /// <param name="recipientId">The unique identifier of the recipient whose unread message count is to be retrieved. Cannot be null or
+        /// empty.</param>
+        /// <remarks>A task that represents the asynchronous operation. The task result contains the number of unread messages
+        /// for the specified recipient.</remarks>
+        public Task<int> GetUnreadCountAsync(string recipientId);
+
+        /// <summary>
+        /// Asynchronously retrieves a notification with the specified identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the notification to retrieve. Must be greater than zero.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the notification with the
+        /// specified identifier, or <see langword="null"/> if no matching notification is found.</returns>
         public Task<Notification?> GetNotificationById(int id);
+
+        /// <summary>
+        /// Asynchronously adds a collection of notifications to the underlying data store.
+        /// </summary>
+        /// <param name="notifications">The collection of notifications to add. Cannot be null. Each notification in the collection must not be
+        /// null.</param>
+        /// <returns>A task that represents the asynchronous add operation.</returns>
         public Task AddRangeAsync(IEnumerable<Notification> notifications);
+
+        /// <summary>
+        /// Marks the specified notification as viewed for the given recipient asynchronously.
+        /// </summary>
+        /// <param name="notificationId">The unique identifier of the notification to mark as viewed.</param>
+        /// <param name="recipientId">The identifier of the recipient for whom the notification is being marked as viewed. Cannot be null or
+        /// empty.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public Task MarkViewedAsync(int notificationId, string recipientId);
 
+        /// <summary>
+        /// Archives the specified notification for the given user asynchronously.
+        /// </summary>
+        /// <param name="notificationId">The unique identifier of the notification to archive.</param>
+        /// <param name="userId">The identifier of the user for whom the notification will be archived. Cannot be null or empty.</param>
+        /// <param name="isAdmin">A value indicating whether the operation is performed with administrative privileges. If <see
+        /// langword="true"/>, the operation may bypass certain user-specific restrictions.</param>
+        /// <returns>A task that represents the asynchronous archive operation.</returns>
         Task ArchiveNotificationAsync(int notificationId, string userId, bool isAdmin);
+
+        /// <summary>
+        /// Restores a previously deleted notification for the specified user.
+        /// </summary>
+        /// <param name="notificationId">The unique identifier of the notification to restore.</param>
+        /// <param name="userId">The identifier of the user for whom the notification will be restored. Cannot be null or empty.</param>
+        /// <param name="isAdmin">A value indicating whether the operation is performed with administrative privileges. Set to <see
+        /// langword="true"/> to restore notifications as an administrator; otherwise, <see langword="false"/>.</param>
+        /// <returns>A task that represents the asynchronous restore operation.</returns>
         Task RestoreNotificationAsync(int notificationId, string userId, bool isAdmin);
     }
 }
