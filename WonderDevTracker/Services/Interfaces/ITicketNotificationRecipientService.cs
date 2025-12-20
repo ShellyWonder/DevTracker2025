a7e16d990d5b8851a3b0cffeb637f161810bceeb
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-12-20 13:31:40 -0600

    Simple name change for specificity;

---
+++ WonderDevTracker/Services/Interfaces/ITicketNotificationRecipientService.cs
@@ -0,0 +1,13 @@
+﻿using WonderDevTracker.Client;
+
+namespace WonderDevTracker.Services.Interfaces
+{
+    public interface ITicketNotificationRecipientService
+    {
+        public Task<string?> GetProjectManagerRecipientAsync(int projectId, UserInfo actor);
+
+        public Task<string?> GetAssignedDeveloperRecipient(int projectId, string assignedUserId, UserInfo actor);
+
+        public string? GetSubmitterRecipient(string? submitterUserId, UserInfo actor);
+    }
+}
