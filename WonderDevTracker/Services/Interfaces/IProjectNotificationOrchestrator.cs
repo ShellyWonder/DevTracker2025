71f9985ef5aa32168fa5b4aebceca1abe01dad0d
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-03-31 20:52:21 -0500

    added interfaces;

---
+++ WonderDevTracker/Services/Interfaces/IProjectNotificationOrchestrator.cs
@@ -0,0 +1,59 @@
+﻿using WonderDevTracker.Client;
+
+namespace WonderDevTracker.Services.Interfaces
+{
+    public interface IProjectNotificationOrchestrator
+    {
+        /// <summary>
+        /// Project Member Added Notification
+        /// </summary>
+        /// <param name="projectId">The project id.</param>
+        /// <param name="addedUserId">The identifier of the user to whom the project is being assigned. Cannot be null or empty.</param>
+        /// <param name="actor">Current user's claims. Cannot be null.</param>
+        /// <returns>Handles the project assignment to a specified user asynchronously..</returns>
+        Task ProjectMemberAddedAsync(int projectId, string addedUserId, UserInfo actor);
+
+        /// <summary>
+        /// project Member Removed Notification
+        /// </summary>
+        /// <param name="projectId">project id.</param>
+        /// <param name="removedUserId">The user ID of the developer removed from the project. Cannot be null or empty.</param>
+        /// <param name="actor">Current user who performed the unassignment action. Cannot be null.</param>
+        /// <remarks>Handles notification when a project is unassigned from a developer.</remarks>
+        Task ProjectMemberRemovedAsync(int projectId, string removedUserId, UserInfo actor);
+
+        /// <summary>
+        /// Notifies the system that a project manager has been assigned to a project.
+        /// </summary>
+        /// <param name="projectId">The unique identifier of the project to which the manager is being assigned.</param>
+        /// <param name="pmUserId">Project manager id. Cannot be null or empty.</param>
+        /// <param name="actor">Information about the user performing the assignment operation. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task ProjectManagerAssignedAsync(int projectId, string pmUserId, UserInfo actor);
+
+        /// <summary>
+        /// Handles the removal of a project manager from the specified project asynchronously.
+        /// </summary>
+        /// <param name="projectId">Project id from which the project manager is being removed.</param>
+        /// <param name="previousPmUserId">The id of the project manager who is being removed. Cannot be null or empty.</param>
+        /// <param name="actor">Information about the user performing the removal action. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task ProjectManagerRemovedAsync(int projectId, string previousPmUserId, UserInfo actor);
+
+        /// <summary>
+        /// Project Archived Notification
+        /// </summary>
+        /// <param name="projectId">project id</param>
+        /// <param name="user">Current user who archived project. Cannot be null.</param>
+        /// <remarks>Handles project archive notification to assignee.</remarks>
+        public Task ProjectArchivedAsync(int projectId, UserInfo user);
+
+        /// <summary>
+        /// Project Restored Notification
+        /// </summary>
+        /// <param name="projectId">project id. Must correspond to an existing archived project.</param>
+        /// <param name="user">Information about the user performing the restore operation. Cannot be null.</param>
+        /// <remarks>Restores a previously archived project with the specified identifier on behalf of the given user.</remarks>
+        public Task ProjectRestoredAsync(int projectId, UserInfo user);
+    }
+}
---
+++ WonderDevTracker/Services/Interfaces/IProjectNotificationRecipientService.cs
@@ -0,0 +1,50 @@
+﻿using WonderDevTracker.Client;
+
+namespace WonderDevTracker.Services.Interfaces
+{
+    public interface IProjectNotificationRecipientService
+    {
+        /// <summary>
+        /// Asynchronously retrieves the recipient identifier for the project manager associated with the specified
+        /// project.
+        /// </summary>
+        /// <param name="projectId">The unique identifier of the project for which to retrieve the project manager recipient.</param>
+        /// <param name="actor">The user requesting the recipient information. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the recipient identifier as a
+        /// string, or null if no project manager is found.</returns>
+        Task<string?> GetProjectManagerRecipientAsync(int projectId, UserInfo actor);
+
+        /// <summary>
+        /// Gets the recipient identifier for the affected member based on the specified user ID and actor information 
+        /// Relies on in-memory logic - no repository call.
+        /// </summary>
+        /// <param name="userId">The unique identifier of the user whose recipient information is to be retrieved. Can be null to indicate an
+        /// anonymous or system-level operation.</param>
+        /// <param name="actor">The user information representing the actor performing the operation. Cannot be null.</param>
+        /// <returns>A string containing the recipient identifier for the affected member, or null if no recipient is applicable.</returns>
+        string? GetAffectedMemberRecipient(string? userId, UserInfo actor);
+
+        /// <summary>
+        /// Asynchronously retrieves the list of all recipient members of the specified project.
+        /// </summary>
+        /// <param name="projectId">The unique identifier of the project whose member recipients are to be retrieved.</param>
+        /// <param name="actor">The user requesting the recipient list. Cannot be null.</param>
+        /// <returns>A list of the project's members. The list is empty if the project has no members.</returns>
+        Task<List<string>> GetProjectMemberRecipientsAsync(int projectId, UserInfo actor);
+
+        /// <summary>
+        /// Asynchronously retrieves the user IDs of all project members who are eligible to receive notifications,
+        /// excluding the specified users.
+        /// </summary>
+        /// <param name="projectId">The unique identifier of the project whose member recipients are to be retrieved.</param>
+        /// <param name="actor">The user performing the operation. This parameter is used to determine access permissions and context.</param>
+        /// <param name="excludedUserIds">An array of user IDs to exclude from the recipient list. Any user ID specified in this array will not be
+        /// included in the returned results. Elements may be null.</param>
+        /// <returns>A task result list containing a list of user IDs for project
+        /// members who are eligible recipients, excluding those specified in the excludedUserIds parameter.</returns>
+        Task<List<string>> GetProjectMemberRecipientsExcludingAsync(
+            int projectId,
+            UserInfo actor,
+            params string?[] excludedUserIds);
+    }
+}
