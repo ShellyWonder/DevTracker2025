9ba7d145b65678001ffdc059c4c2950f74c696f3
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-04-20 12:11:42 -0500

    added helper to standardize timestamps(future implementation) throughout app.

---
+++ WonderDevTracker.Client/Helpers/DateTimeHelper.cs
@@ -0,0 +1,44 @@
+﻿using WonderDevTracker.Client.Models.DTOs;
+
+namespace WonderDevTracker.Client.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static string FormatTimestamp(DateTimeOffset created)
+        {
+            var now = DateTimeOffset.Now;
+            var local = created.ToLocalTime();
+            var diff = now - local;
+
+            if (diff.TotalSeconds < 60)
+                return "Just now";
+
+            if (diff.TotalMinutes < 60)
+                return $"{(int)diff.TotalMinutes}m ago";
+
+            if (diff.TotalHours < 24)
+                return $"{(int)diff.TotalHours}h ago";
+
+            if (local.Date == now.Date.AddDays(-1))
+                return "Yesterday";
+
+            if (local.Date == now.Date)
+                return $"Today, {local:t}";
+
+            if (local.Year == now.Year)
+                return local.ToString("MMM d");
+
+            return local.ToString("MMM d, yyyy");
+        }
+
+
+            private static string GetCommentTimestamp(TicketCommentDTO comment)
+            {
+                //if (comment.Edited == true)
+                //return $"Edited {DateTimeHelper.FormatTimestamp(comment.Edited.Value)}";
+
+                return DateTimeHelper.FormatTimestamp(comment.Created);
+            }
+    }
+}
+
