19427bfb0ea89a469545c4d5495a5a38fe1952e3
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-05-07 21:03:25 -0500

    Modified class name to clarify function and added needed properties; Modified Dashboard.razor to reflect changes;

--- WonderDevTracker.Client/Models/ViewModels/DashboardActionPermissionsViewModel.cs
+++ WonderDevTracker.Client/Models/ViewModels/DashboardActionPermissionsViewModel.cs
@@ -1,11 +1,16 @@
 ﻿namespace WonderDevTracker.Client.Models.ViewModels
 {
     // This class encapsulates the permissions for various dashboard actions based on the user's roles and project memberships.
-    public class DashboardActionPermissions
+    public class DashboardActionPermissionsViewModel
     {
         public bool CanManageProjects { get; set; }
         public bool CanManageTickets { get; set; }
         public bool CanManageMembers { get; set; }
         public bool CanSubmitTickets { get; set; }
+        public bool CanViewInvites { get; set; }
+        public bool CanViewUnassignedTickets { get; set; }
+        public bool CanViewArchivedTickets { get; set; }
+
+
     }
 }
