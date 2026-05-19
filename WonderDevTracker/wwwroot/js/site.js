c4d1b046fdcc77c7d47f4edbe85aac8bc1688891
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-05-19 11:55:45 -0500

    Added site.js to help resolve page rendering timing issue for CompanyDetails.razor's target section navigation;

---
+++ WonderDevTracker/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿//site.js
+//allows rendering before scroll when usings anchor tags; 
+window.scrollToFragment = (fragment) => {
+    if (!fragment) return;
+
+    const id = fragment.startsWith("#")
+        ? fragment.substring(1)
+        : fragment;
+
+    const element = document.getElementById(id);
+
+    if (element) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+};
