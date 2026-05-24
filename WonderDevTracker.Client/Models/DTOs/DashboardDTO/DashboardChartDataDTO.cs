4324434218ffeff7da999e5c7250f7676b261f98
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-05-24 09:01:04 -0500

    Created TicketsByProjectChartDTO.cs to support the TicketsByProjectChart.razor;
    Updated DashboardChartDataDTO. cs;

--- WonderDevTracker.Client/Models/DTOs/DashboardDTO/DashboardChartDataDTO.cs
+++ WonderDevTracker.Client/Models/DTOs/DashboardDTO/DashboardChartDataDTO.cs
@@ -6,5 +6,6 @@
         public List<DashboardCountByCategoryDTO> TicketsByStatus { get; set; } = [];
         public List<DashboardCountByCategoryDTO> TicketsByPriority { get; set; } = [];
         public List<DashboardCountByCategoryDTO> ProjectsByPriority { get; set; } = [];
+        public List<DashboardTicketsByProjectDTO> TicketsByProject { get; set; } = [];
     }
 }
