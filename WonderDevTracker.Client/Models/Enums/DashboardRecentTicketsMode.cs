400ac7f46a327e1b1fffff2e31e0c6f1dd689215
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-06-03 08:11:42 -0500

    Added enum to improve flexibility on DashBoardRecentTickets; Enabled single tab use on PM dashboard Unassigned Tickets table.

---
+++ WonderDevTracker.Client/Models/Enums/DashboardRecentTicketsMode.cs
@@ -0,0 +1,12 @@
+﻿namespace WonderDevTracker.Client.Models.Enums
+{
+    /// <summary>
+    /// Specifies the mode for displaying recent tickets on the dashboard.
+    /// </summary>
+    public enum DashboardRecentTicketsMode
+    {
+   
+        Tabs,
+        SingleUnassigned
+    }
+}
