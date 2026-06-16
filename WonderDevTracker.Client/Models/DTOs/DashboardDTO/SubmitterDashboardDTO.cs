61aacf368724831ebf89ea5be148d4cb3659a08f
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-06-16 17:43:17 -0500

    Made repo and DTO changes to accomodate new SubmitterDashboardDTO.cs

--- WonderDevTracker.Client/Models/DTOs/DashboardDTO/SubmitterDashboardDTO.cs
+++ WonderDevTracker.Client/Models/DTOs/DashboardDTO/SubmitterDashboardDTO.cs
@@ -3,6 +3,12 @@ using System.ComponentModel;
 
 namespace WonderDevTracker.Client.Models.DTOs.DashboardDTO
 {
+    /// <summary>
+    /// DTO for submitter role-specific dashboard data, 
+    /// including stats and lists of tickets submitted by the current user. 
+    /// Since any user can submit tickets, 
+    /// the other role DTOs have their own submitted ticket list properties.
+    /// </summary>
     public class SubmitterDashboardDTO
     {
         [Description("Stats for submitters on the dashboard.")]
