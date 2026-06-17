7a8d52bdc7dff895d0c9053ffd3c9ad07500d532
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-06-17 07:49:10 -0500

    Created AdminDashboardDTO to bring the admin business logic in line with the other roles; Updated DashboardDTO to reflect the changes;

---
+++ WonderDevTracker.Client/Models/DTOs/DashboardDTO/AdminDashboardDTO.cs
@@ -0,0 +1,40 @@
+﻿//AdminDashboardDTO
+using System.ComponentModel;
+
+namespace WonderDevTracker.Client.Models.DTOs.DashboardDTO
+{
+    public class AdminDashboardDTO
+
+    {
+        #region Stats for dashboard cards
+        [Description("General stats for company dashboard, primarily for admin consumption.")]
+        public CompanyDashboardStatsDTO CompanyStats { get; set; } = new();
+
+        #region Dashboard Ticket Lists for tables
+        [Description("List of recent active tickets on the dashboard.")]
+        public List<DashboardTicketSummaryDTO> RecentActiveTickets { get; set; } = [];
+
+        [Description("List of recent resolved tickets on the dashboard.")]
+        public List<DashboardTicketSummaryDTO> RecentResolvedTickets { get; set; } = [];
+
+        [Description("List of recent unassigned tickets on the dashboard.")]
+        public List<DashboardTicketSummaryDTO> RecentUnassignedTickets { get; set; } = [];
+
+        [Description("List of tickets submitted by the current user.")]
+        public List<DashboardTicketSummaryDTO> MySubmittedTickets { get; set; } = [];
+        #endregion
+
+        #region Chart data for dashboard charts
+
+        [Description("Data for charts on the dashboard.")]
+        public DashboardChartDataDTO ChartData { get; set; } = new();
+
+        #endregion
+
+        #region Dashboard Projects
+        [Description("List of projects to display on the dashboard.")]
+        public List<DashboardProjectSummaryDTO> RecentProjects { get; set; } = [];
+        #endregion
+
+    }
+}
--- WonderDevTracker.Client/Models/DTOs/DashboardDTO/DashboardDTO.cs
+++ WonderDevTracker.Client/Models/DTOs/DashboardDTO/DashboardDTO.cs
@@ -12,50 +12,18 @@ namespace WonderDevTracker.Client.Models.DTOs.DashboardDTO
         [Description("Basic info about the company, used for display and to determine what data to load.")]
         public CompanyDashboardInfoDTO CompanyInfo { get; set; } = new();
 
-        #region Stats for dashboard cards
-        [Description("General stats for company dashboard, primarily for admin consumption.")]
-        public CompanyDashboardStatsDTO CompanyStats { get; set; } = new();
-
         #region Dashboards by role
+        [Description("Dashboard data for administrators.")]
+        public AdminDashboardDTO AdminDashboard { get; set; } = new();
+
         [Description("Dashboard data for project managers.")]
         public PMDashboardDTO PMDashboard { get; set; } = new();
 
         [Description("Dashboard data for developers.")]
         public DeveloperDashboardDTO DevDashboard { get; set; } = new();
-
-
+        
         [Description("Dashboard data for submitters.")]
         public SubmitterDashboardDTO SubmitterDashboard { get; set; } = new();
-        #endregion
-
-        
-        #endregion
-
-        #region Dashboard Projects
-        [Description("List of projects to display on the dashboard.")]
-        public List<DashboardProjectSummaryDTO> RecentProjects { get; set; } = [];
-        #endregion
-
-        #region Dashboard Ticket Lists for tables
-        [Description("List of recent active tickets on the dashboard.")]
-        public List<DashboardTicketSummaryDTO> RecentActiveTickets { get; set; } = [];
-
-        [Description("List of recent resolved tickets on the dashboard.")]
-        public List<DashboardTicketSummaryDTO> RecentResolvedTickets { get; set; } = [];
-
-        [Description("List of recent unassigned tickets on the dashboard.")]
-        public List<DashboardTicketSummaryDTO> RecentUnassignedTickets { get; set; } = [];
-
-        [Description("List of tickets submitted by the current user.")]
-        public List<DashboardTicketSummaryDTO> MySubmittedTickets { get; set; } = [];
-        #endregion
-
-        #region Chart data for dashboard charts
-
-        [Description("Data for charts on the dashboard.")]
-        public DashboardChartDataDTO ChartData { get; set; } = new();
-
-
         #endregion
 
     }
